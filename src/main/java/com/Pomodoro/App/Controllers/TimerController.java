62efee3bbc3653768dbe50c24e427844583c60e3
CaioAlexndre <caioa211dos@gmail.com>
2025-04-07 18:49:21 -0300

    Correcao:retorno dos metodos

--- src/main/java/com/Pomodoro/App/Controllers/TimerController.java
+++ src/main/java/com/Pomodoro/App/Controllers/TimerController.java
@@ -1,16 +1,25 @@
 package com.Pomodoro.App.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping()
-@RestController
+@Controller
 public class TimerController
 {
 //Para nao esquecer: postmapping significa que estou aguardando receber os dados enviados via post
     //devo ter uma rota valida para isso.
+
+
+    @GetMapping("/Home")
+    public String home()
+    {
+        return "TimerPage";
+    }
+
     @PostMapping("/NewCycle")
     public void saveStudyCycle()
     {
@@ -19,9 +28,9 @@ public class TimerController
 
     //para nao esquecer: estou indo pegar os dados.
     @GetMapping("/RecoveryCycle")
-    public void recoveryStudyCycle()
+    public String recoveryStudyCycle()
     {
-
+    return null;
     }
 
     @PostMapping("/RemoveCycle")
